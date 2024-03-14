aa17bfa28951c168675ec9312f411d5d6e42f4e5
simondarius <simondarius0@gmail.com>
2024-03-14 17:39:50 +0200

    Setup working camera

--- app/src/main/java/com/cnidaria/kissvision/MainActivity.java
+++ app/src/main/java/com/cnidaria/kissvision/MainActivity.java
@@ -9,6 +9,14 @@ import androidx.core.app.ActivityCompat;
 import android.content.Context;
 import android.content.pm.PackageManager;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.hardware.camera2.CameraAccessException;
 
 import android.hardware.camera2.CameraManager;
@@ -18,19 +26,25 @@ import android.media.ImageReader;
 
 import android.os.Bundle;
 
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.Size;
 import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity implements ImageReader.OnImageAvailableListener {
     private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
 
-
-
-
-
-
+    private ModelRunner modelRunner;
 
 
     @Override
@@ -45,6 +59,11 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
         } else {
             //TODO show live camera footage
             setFragment();
+        }
+        try {
+            modelRunner = new ModelRunner(this);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     @Override
@@ -101,11 +120,178 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
                 return 0;
         }
     }
+    private boolean isProcessingFrame = false;
+    private byte[][] yuvBytes = new byte[3][];
+    private int[] rgbBytes = null;
+    private int yRowStride;
+    private Runnable postInferenceCallback;
+    private Runnable imageConverter;
+    private Bitmap rgbFrameBitmap;
     @Override
     public void onImageAvailable(ImageReader reader) {
-        final Image image = reader.acquireLatestImage();
-        Log.w("Quality","GOT AN IMAGE!");
-        image.close();
+        // We need wait until we have some size from onPreviewSizeChosen
+        if (previewWidth == 0 || previewHeight == 0) {
+            return;
+        }
+        if (rgbBytes == null) {
+            rgbBytes = new int[previewWidth * previewHeight];
+        }
+        try {
+            final Image image = reader.acquireLatestImage();
+
+            if (image == null) {
+                return;
+            }
+
+            if (isProcessingFrame) {
+                image.close();
+                return;
+            }
+            isProcessingFrame = true;
+            final Image.Plane[] planes = image.getPlanes();
+            fillBytes(planes, yuvBytes);
+            yRowStride = planes[0].getRowStride();
+            final int uvRowStride = planes[1].getRowStride();
+            final int uvPixelStride = planes[1].getPixelStride();
+
+            imageConverter =
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            ImageUtils.convertYUV420ToARGB8888(
+                                    yuvBytes[0],
+                                    yuvBytes[1],
+                                    yuvBytes[2],
+                                    previewWidth,
+                                    previewHeight,
+                                    yRowStride,
+                                    uvRowStride,
+                                    uvPixelStride,
+                                    rgbBytes);
+                        }
+                    };
+
+            postInferenceCallback =
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            image.close();
+                            isProcessingFrame = false;
+                        }
+                    };
+
+            processImage();
+
+        } catch (final Exception e) {
+            Log.d("tryError",e.getMessage());
+            return;
+        }
+
+    }
+    private void drawBoundingBox(final float[] results) {
+        // Post a Runnable to the main thread
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // Extract bounding box coordinates from results array
+                float xmin = results[0];
+                float ymin = results[1];
+                float xmax = results[2];
+                float ymax = results[3];
+
+                // Get the device's screen width and height
+                WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+                DisplayMetrics displayMetrics = new DisplayMetrics();
+                windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+                int screenWidth = displayMetrics.widthPixels;
+                int screenHeight = displayMetrics.heightPixels;
+
+                // Scale the bounding box coordinates to match the original screen size
+                float scaleX = (float) screenWidth / 128f;
+                float scaleY = (float) screenHeight / 128f;
+                float scaledXmin = xmin * scaleX;
+                float scaledYmin = ymin * scaleY;
+                float scaledXmax = xmax * scaleX;
+                float scaledYmax = ymax * scaleY;
+
+                // Draw the square on the ImageView
+                ImageView imageView = findViewById(R.id.imageView);
+                Drawable drawable = imageView.getDrawable();
+                if (drawable == null) {
+                    // Set a transparent background color
+                    imageView.setBackgroundColor(Color.TRANSPARENT);
+                }
+                try{
+                Bitmap bitmap = Bitmap.createBitmap(imageView.getWidth(), imageView.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                Paint paint = new Paint();
+                paint.setColor(Color.RED);
+                paint.setStyle(Paint.Style.STROKE);
+                paint.setStrokeWidth(5);
+                canvas.drawRect(scaledXmin, scaledYmin, scaledXmax, scaledYmax, paint);
+                System.out.println("Success!");
+                imageView.setImageBitmap(bitmap);}catch (Exception e){
+                    System.out.println("Fail!");
+                    Log.e("BoundingBoxDraw","Exception: "+e.getMessage());
+                }
+            }
+        });
+    }
+    private void processImage() {
+        imageConverter.run();
+        rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Bitmap.Config.ARGB_8888);
+        rgbFrameBitmap.setPixels(rgbBytes, 0, previewWidth, 0, 0, previewWidth, previewHeight);
+        //Do your work here
+        Log.w("Quality!","Aight G!");
+        float[] results=modelRunner.runInference(bitmapToFloatArray(rgbFrameBitmap));
+        drawBoundingBox(results);
+        Log.w("Great success!", Arrays.toString(results));
+        postInferenceCallback.run();
+    }
+
+    protected void fillBytes(final Image.Plane[] planes, final byte[][] yuvBytes) {
+        for (int i = 0; i < planes.length; ++i) {
+            final ByteBuffer buffer = planes[i].getBuffer();
+            if (yuvBytes[i] == null) {
+                yuvBytes[i] = new byte[buffer.capacity()];
+            }
+            buffer.get(yuvBytes[i]);
+        }
+    }
+    private float[][][][] bitmapToFloatArray(Bitmap bitmap) {
+        if (bitmap == null) {
+            return new float[0][0][0][0]; // Return an empty tensor if the input bitmap is null
+        }
+
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, 128, 128, true);
+
+        if (scaledBitmap == null) {
+            return new float[0][0][0][0]; // Return an empty tensor if the scaled bitmap is null
+        }
+
+        float[][][][] floatValues = new float[1][128][128][3];
+
+        for (int y = 0; y < scaledBitmap.getHeight(); y++) {
+            for (int x = 0; x < scaledBitmap.getWidth(); x++) {
+                int color = scaledBitmap.getPixel(x, y);
+                float red = ((color >> 16) & 0xFF) / 255.0f;
+                float green = ((color >> 8) & 0xFF) / 255.0f;
+                float blue = (color & 0xFF) / 255.0f;
+                floatValues[0][y][x][0] = red;
+                floatValues[0][y][x][1] = green;
+                floatValues[0][y][x][2] = blue;
+            }
+        }
+
+        return floatValues;
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Close the TensorFlow Lite model runner
+        if (modelRunner != null) {
+            modelRunner.close();
+        }
     }
 
 }
